4922b37329d99bf00d48acab46b2976e69d17a93
BrettLennox <brettheaton@hotmail.com>
2022-05-20 17:33:45 +1000

    Added NavMeshVolume
    Forgot to add NavMeshVolume into the assignment
    
    -added area in hallway that slows AI when they walk through it

--- Assets/Scripts/AI/AIAgent.cs
+++ Assets/Scripts/AI/AIAgent.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public abstract class AIAgent : MonoBehaviour
 {
     #region Variables
+    private float _walkSpeed = 3f, _slowWalkSpeed = 1.5f;
     protected NavMeshAgent _agent => GetComponent<NavMeshAgent>();
     private Animator _anim => GetComponentInChildren<Animator>();
     #endregion
@@ -13,9 +14,27 @@ public abstract class AIAgent : MonoBehaviour
     protected virtual void Update()
     {
         AnimationState();
+        AdjustMoveSpeed();
     }
 
     protected virtual void MoveToTarget(Transform target) => _agent.SetDestination(target.position); //sets the Agents destination to the passed in target
+
+    protected virtual void AdjustMoveSpeed() //adjusts walk speed based off of the NavMeshMask registered when moving on the NavMesh
+    {
+        NavMeshHit navHit;
+        _agent.SamplePathPosition(-1, 0.0f, out navHit);
+
+        int SlowWalkMask = 1 << NavMesh.GetAreaFromName("SlowWalk");
+
+        if (navHit.mask == SlowWalkMask) 
+        { 
+            _agent.speed = _slowWalkSpeed; 
+        }
+        else
+        {
+            _agent.speed = _walkSpeed;
+        }
+    }
 
     protected virtual bool CheckPath() //returns based on Agent Path Status
     {
